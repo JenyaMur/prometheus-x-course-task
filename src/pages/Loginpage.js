75a6801e5b66f10985caf73cca65bc8f8ea8ba47
JenyaMur <smp-2000@ukr.net>
2023-02-17 11:42:13 +0100

    change form validation for login page

--- src/pages/Loginpage.js
+++ src/pages/Loginpage.js
@@ -1,46 +1,41 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import {useForm} from 'react-hook-form';
+import { useAuth } from "../hook/useAuth";
 import avatar from '../img/avatar.png';
 import avatar1 from '../img/avatar1.jpg';
 import avatar2 from '../img/avatar2.png';
 import avatar3 from '../img/avatar3.png';
 import avatar4 from '../img/avatar4.jpg';
-import { useAuth } from "../hook/useAuth";
 import './Loginpage.sass';
 
 export default function Loginpage() {
     const navigate = useNavigate();
     const {user, signIn, foto, installFoto} = useAuth();
-    const [disabled, setDisabled] = useState(true);
+    const {register, formState: {errors, isValid}, handleSubmit } = useForm({mode: "all"});
+
     useEffect(() => {user && navigate('/')});
-    const hadleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const user = form.username.value.trim();
-        if(user.length > 3 && user.length < 17) {
-            signIn(user, () => {
-                navigate('/', { replace: true });
-                localStorage.setItem('userName', user)
-            });
+
+    const changeFoto = ({target: {alt, src}}) => {
+        if(alt === "profile_avatar") {
+            installFoto(src);
+            localStorage.setItem('foto', src)
         }
     }
-    const controlBtn = (e) => {
-        e.target.value.trim().length > 3 && e.target.value.trim().length < 17 ? setDisabled(false) : setDisabled(true);
-    }
-    const changeFoto = (e) => {
-        if(e.target.alt === "profile_avatar") {
-            installFoto(e.target.src);
-            localStorage.setItem('foto', e.target.src)
-        } 
+    function createUser({username}) {
+        signIn(username, () => {
+            navigate('/', { replace: true });
+            localStorage.setItem('userName', username)
+        });
     }
 
     return (
         <div className="parent-block">
             <div className="sign-up-block">
                 <img
-                src={foto}
-                alt="profile_avatar"
-                className="img-fluid main-photo-avatar photo-avatar"
+                    src={foto}
+                    alt="profile_avatar"
+                    className="img-fluid main-photo-avatar photo-avatar"
                 />
                 <div className="mt-3 mb-3" onClick={changeFoto}>
                     <p className="text-center mt-3 mb-3">You can select a profile photo:</p>
@@ -50,17 +45,28 @@ export default function Loginpage() {
                     <img src={avatar4} alt="profile_avatar" className="img-fluid photo-avatar"/>
                     <img src={avatar} alt="profile_avatar" className="img-fluid photo-avatar"/>
                 </div>
-                <form onSubmit={hadleSubmit} className="text-center mt-3">
+                <form onSubmit={handleSubmit(createUser)} className="text-center mt-3">
                     <input
-                        type="text"
-                        id="user_name"
-                        name="username"
                         placeholder="Type your name..."
-                        onChange={controlBtn}
-                        className="form-control"
+                        className="form-control mb-3"
+                        {...register("username", {
+                            required: "You should type your name",
+                            pattern: {
+                                value: /^\w{4,16}$/,
+                                message: 'You can use: english letters, numbers or _' 
+                            },
+                            minLength: {
+                                value: 4,
+                                message: "Minimum 4 symbols. You can use: english letters, numbers or _" 
+                            },
+                            maxLength: {
+                                value: 16,
+                                message: "Maximum 16 symbols. You can use: english letters, numbers or _"
+                            }
+                        })}
                     />
-                    <p className="mt-1 login-clue">Name should have 4-16 symbols </p>
-                    <button type="submit" className="btn btn-primary" disabled={disabled}>
+                    {errors?.username && <p className="mt-1 login-clue">{errors?.username.message}</p>}
+                    <button type="submit" className="btn btn-primary" disabled={!isValid}>
                         Sign-in
                     </button>
                 </form>
